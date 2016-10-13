b3f4115fbc22b56a235546ba09ae8528e221c140
Jackson Ward <jward93@gmail.com>
2016-10-12 23:09:51 -0500

    added usage statement and comments to exampleProgram. Updated makefile to build exampleProgram

--- Makefile
+++ Makefile
@@ -7,7 +7,12 @@
 CC = gcc
 CFLAGS = -Wall -g -DRUN_TESTS
 
+default: clean
 default: romanNumeralCalculatorTest
+
+example: clean
+example: CFLAGS = -Wall 
+example: exampleProgram
 
 romanNumeralCalculatorTest: RomanNumeralCalculator.o RomanNumeralCalculator_TB.o
 	$(CC) $(CFLAGS) -o runTests RomanNumeralCalculator.o RomanNumeralCalculator_TB.o
@@ -18,5 +23,11 @@ RomanNumeralCalculator.o: RomanNumeralCalculator.c RomanNumeralCalculator.h
 RomanNUmeralCalculator_TB.o: RomanNumeralCalculator.c RomanNumeralCalculator.h RomanNumeralCalculator_TB.c RomanNumeralCalculator_TB.h
 	$(CC) $(CFLAGS) -c RomanNumeralCalculator_TB.c
 
+exampleProgram: RomanNumeralCalculator.o exampleProgram.o
+	$(CC) $(CFLAGS) -o exampleProgram exampleProgram.o RomanNumeralCalculator.o
+
+exampleProgram.o: exampleProgram.c
+	$(CC) $(CFLAGS) -c exampleProgram.c
+
 clean:
-	$(RM) runTests *.o *~
+	$(RM) runTest exampleProgram *.o *~
--- exampleProgram.c
+++ exampleProgram.c
@@ -1,28 +1,44 @@
+   // FILE: exampleProgram.c
+   // AUTHOR: Jackson Ward
+   // 
+   // DESCRIPTION: Short program to demonstrate use of
+   //              the RomanNumeralCalculator Library.
+   //              Add or Subtract Numerals from
+   //              Command Line
+   //
+   // CREATED ON: 10/12/16
+
    #include <stdio.h>
    #include "RomanNumeralCalculator.h"
 
    int main(int i, char *v[])
    {
-      char returnChar;
-      char result[STRING_SIZE];
+      if (i != 4  ||  (*v[1] != 'a' && *v[1] != 's')) // check for proper usage, print message
+      {
+         printf("Usage: PROGRAM <function> <operand_1> <operand_2>\n\nFunction = a for add or s for subtract. Example: 'exampleProgram a XIV CCI'\n\n");
+         return 0;
+      }
+  
+      char returnChar;           // stores return character
+      char result[STRING_SIZE];  // stores result string
 
-      if (*v[1] == 'a')
+      if (*v[1] == 'a')   // add function
       {
          returnChar = addNumerals(v[2], v[3], &result[0]);
       }
 
-      else if (*v[1] == 's')
+      else if (*v[1] == 's')  // subtract function
       {
          returnChar = subtractNumerals(v[2], v[3], &result[0]);
       }
 
-      if (returnChar == 'I') printf("Invalid Input!\n");
+      if (returnChar == 'I') printf("Invalid Input!\n");  // alert invalid input
     
-      else if (returnChar == 'X') printf("Result = %s | Overflow Occured!\n", result);
+      else if (returnChar == 'X') printf("Result = %s | Overflow Occured!\n", result);  // print result and alert overflow
 
-      else if (returnChar == 'V') printf("Result = Negative %s\n", result);
+      else if (returnChar == 'V') printf("Result = Negative %s\n", result);   // print result and alert negative
 
-      else printf("Result = %s\n", result);
+      else printf("Result = %s\n", result);  // print result
 
       
       return 0;
